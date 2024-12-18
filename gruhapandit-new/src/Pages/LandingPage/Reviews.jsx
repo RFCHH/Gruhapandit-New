6b2bab58ce568476606076ed03adf1ea569c35b6
saikiran114 <saikiran.rfchh@gmail.com>
2024-12-18 11:25:23 +0530

    Reviews and createpassword page
    completed

--- gruhapandit-new/src/Pages/LandingPage/Reviews.jsx
+++ gruhapandit-new/src/Pages/LandingPage/Reviews.jsx
@@ -1,11 +1,199 @@
-import React from 'react'
+import React from "react";
+import message from "../../assets/24.png";
+import Mails from "../../assets/23.png";
+import footer from "../../assets/Vector.png";
 
-function Reviews() {
+const HomeReviews = () => {
   return (
-    <div>
-      <h1>Review</h1>
-    </div>
-  )
-}
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <div className="relative flex flex-col items-start min-h-screen px-4 sm:px-12 md:px-24">
+        <div className="flex flex-col gap-2 sm:mt-32 md:mt-56 mt-96">
+          <h1
+            className="text-[32px] sm:text-[40px] md:text-[64px] font-extrabold text-[#32046B] leading-tight text-center sm:text-left"
+            style={{
+              fontFamily: "Inter, sans-serif",
+            }}
+          >
+            Reviews
+          </h1>
+          <p
+            className="text-lg sm:text-2xl md:text-3xl font-semibold text-[#000000] text-center sm:text-left"
+            style={{
+              fontFamily: "Inter, sans-serif",
+            }}
+          >
+            Hire a Tutor Or Find a Student With Better Review
+          </p>
+        </div>
 
-export default Reviews
+        <div className="absolute bottom-0 left-0 w-full -mt-12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1920 721"
+            fill="none"
+            className="w-full h-auto"
+          >
+            <path
+              d="M-193 320.298L-69.4167 380.271C54.1667 441.369 301.333 569.625 548.5 540.624C795.667 510.497 1042.83 320.298 1290 290.171C1537.17 261.17 1784.33 379.426 2031.5 365.348C2278.67 351.27 2525.83 209.227 2649.42 155.1767L2773 0V720.795H2649.42C2525.83 720.795 2278.67 720.795 2031.5 720.795C1784.33 720.795 1537.17 720.795 1290 720.795C1042.83 720.795 795.667 720.795 548.5 720.795C301.333 720.795 54.1667 720.795 -69.4167 720.795H-193V320.298Z"
+              fill="url(#paint0_radial_33_3955)"
+            />
+            <defs>
+              <radialGradient
+                id="paint0_radial_33_3955"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientUnits="userSpaceOnUse"
+                gradientTransform="translate(921 -618.391) rotate(90) scale(1289.28 5305.27)"
+              >
+                <stop offset="0.266" stopColor="#26A3C9" />
+                <stop offset="1" stopColor="#FAF6FF" />
+              </radialGradient>
+            </defs>
+          </svg>
+        </div>
+      </div>
+
+      <div className="text-center mt-10 mb-8">
+        <h2
+          className="text-[52px] sm:text-[52px] font-bold text-[#32046B] relative"
+          style={{
+            fontFamily: "Readex Pro, sans-serif",
+          }}
+        >
+          Reviews
+        </h2>
+        <div
+          className="w-64 h-[3px] mx-auto mt-4"
+          style={{
+            background:
+              "linear-gradient(89.99deg, #7C14FD 1.09%, rgba(124, 20, 253, 0.29) 99.99%)",
+          }}
+        ></div>
+      </div>
+      <div className="max-w-7xl mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14 relative">
+        {Array(9)
+          .fill()
+          .map((_, index) => (
+            <div
+              key={index}
+              className="rounded-lg p-6 flex flex-col items-start shadow-2xl shadow-blue-300"
+            >
+              <h2 className="text-xl font-bold text-[#000000] mb-2">
+                Arbacheena Bora
+              </h2>
+              <p className="text-xl text-[#000000] mb-4 mt-4 font-bold">
+                Tutor
+              </p>
+              <p className="text-sm text-[#32046B] mb-6">
+                Lorem Ipsum helped us with our digital marketing for 6 months
+                and we are delighted. Great service and highly recommended.
+              </p>
+              {/* <button className="bg-blue-500 text-white px-4 py-2 rounded-lg flex items-center hover:bg-blue-600 transition">
+                <span className="mr-2">Read More</span>
+                <IoIosArrowForward />
+              </button> */}
+            </div>
+          ))}
+      </div>
+      <div className="flex justify-center items-center space-x-1 my-8">
+        <button className="text-[#000000] hover:underline font-bold">
+          Previous
+        </button>
+        {[1, 2, 3, 4, 5].map((number) => (
+          <button
+            key={number}
+            className={`px-4 py-2 rounded-lg transition ${
+              number === 1 ? "text-[#000000]" : "text-[#000000] hover:underline"
+            }`}
+          >
+            {number}
+          </button>
+        ))}
+        <button className="text-[#B070FF] hover:underline font-bold">
+          Next
+        </button>
+      </div>
+      <section className="bg-purple-50 py-12 sm:py-16 relative">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center">
+          <div className="absolute top-2 left-72 text-pink-300 text-2xl">★</div>
+          <div className="absolute top-10 left-20 text-pink-300 text-2xl">
+            ★
+          </div>
+          <div className="absolute top-20 right-32 text-blue-300 text-xl">
+            •
+          </div>
+          <div className="absolute bottom-24 left-16 text-purple-300 text-3xl">
+            ✦
+          </div>
+          <div className="absolute bottom-12 right-24 text-pink-300 text-2xl">
+            •
+          </div>
+          <div className="absolute top-40 left-1/2 transform -translate-x-1/2 text-blue-300 text-lg">
+            ✦
+          </div>
+          <div className="md:w-1/2 text-center md:text-left px-6 sm:px-10">
+            <h1
+              className="text-[#000000] font-bold mb-4 text-3xl sm:text-4xl lg:text-5xl leading-snug"
+              style={{
+                fontFamily: "Inter, sans-serif",
+                fontSize: "40px",
+                fontWeight: "600",
+                lineHeight: "70.34px",
+                textUnderlinePosition: "from-font",
+                textDecorationSkipInk: "none",
+              }}
+            >
+              LET'S KEEP IN TOUCH
+            </h1>
+            <p
+              className="text-[#32046B] font-medium mb-8 text-lg sm:text-xl lg:text-2xl"
+              style={{
+                fontFamily: "Inter, sans-serif",
+                fontSize: "24px",
+                lineHeight: "42.5px",
+                letterSpacing: "0.2px",
+                textUnderlinePosition: "from-font",
+                textDecorationSkipInk: "none",
+              }}
+            >
+              Subscribe to our email newsletter to keep yourself updated.
+            </p>
+
+            <div className="flex flex-col sm:flex-row items-center justify-center md:justify-start gap-4">
+              <div className="relative flex items-center w-full sm:w-72 bg-gradient-to-r from-purple-600 to-violet-800 rounded-full">
+                <img src={message} alt="Mail Icon" className="ml-4 w-8 h-8" />
+                <input
+                  type="email"
+                  placeholder="Enter Your Email..."
+                  className="flex-1 bg-transparent text-white placeholder-white px-4 py-3 focus:outline-none focus:ring-purple-500 rounded-full"
+                />
+              </div>
+              <button
+                className="w-full sm:w-auto px-6 py-3 rounded-full border-2 border-purple-600 text-black bg-white hover:bg-purple-100 transition flex-shrink-0"
+                style={{
+                  height: "3rem",
+                }}
+              >
+                Subscribe Now
+              </button>
+            </div>
+          </div>
+          <div className="md:w-1/2 mt-10 md:mt-0 flex justify-center">
+            <img
+              src={Mails}
+              alt="Newsletter Illustration"
+              className="w-full max-w-md sm:max-w-lg object-contain"
+            />
+          </div>
+        </div>
+      </section>
+
+      <div>
+        <img src={footer} alt="footer icon" />
+      </div>
+    </div>
+  );
+};
+
+export default HomeReviews;
---
+++ gruhapandit-new/src/Pages/SignUp/CreatePassword.jsx
@@ -0,0 +1,301 @@
+import React, { useState } from "react";
+import passwordImg from "../../assets/password.png";
+import { useNavigate } from "react-router-dom";
+import { IoArrowForwardOutline } from "react-icons/io5";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import axios from "axios";
+
+const CreatePassword = () => {
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const validateForm = () => {
+    const newErrors = {};
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!password) {
+      newErrors.password = "Password is required.";
+    } else if (!passwordRegex.test(password)) {
+      newErrors.password =
+        "Password must be at least 8 characters, include uppercase, lowercase, number, and special character.";
+    }
+
+    if (!confirmPassword) {
+      newErrors.confirmPassword = "Confirm password is required.";
+    } else if (password !== confirmPassword) {
+      newErrors.confirmPassword = "Passwords do not match.";
+    }
+
+    if (!selectedOption) {
+      newErrors.dropdown = "Please select how you heard about us.";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const isValid = validateForm();
+    if (!isValid) {
+      console.log("Form validation failed. Please check the inputs.");
+      return;
+    }
+
+    const formDetails = JSON.parse(localStorage.getItem("formDetails"));
+    if (!formDetails) {
+      alert("Signup details are missing. Please sign up again.");
+      return;
+    }
+
+    const {
+      address,
+      type,
+      fullName,
+      emailId,
+      countryCode,
+      mobileNumber,
+      dateOfBirth,
+      gender,
+      country,
+    } = formDetails;
+
+    const postData = {
+      type: type,
+      fullName: fullName,
+      emailId: emailId,
+      countryCode: countryCode,
+      mobileNumber: mobileNumber,
+      dateOfBirth: dateOfBirth,
+      gender: gender,
+      country: country,
+      address: address,
+      password: password,
+    };
+
+    try {
+      const response = await axios.post(
+        `https://tution-application.onrender.com/tuition-application/users/`,
+        postData
+      );
+
+      alert("Password created successfully!");
+      console.log(response.data);
+
+      navigate("/LoginPage");
+    } catch (error) {
+      console.error("Error creating password:", error);
+
+      if (error.response) {
+        alert(
+          `Error: ${
+            error.response.data.message || "Failed to create password."
+          }`
+        );
+      } else {
+        alert("Failed to connect to the server. Please try again.");
+      }
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setConfirmPasswordVisible(!confirmPasswordVisible);
+  };
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    if (newPassword.includes(" ")) {
+      e.preventDefault();
+    } else {
+      setPassword(newPassword);
+    }
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    const newConfirmPassword = e.target.value;
+    if (newConfirmPassword.includes(" ")) {
+      e.preventDefault();
+    } else {
+      setConfirmPassword(newConfirmPassword);
+    }
+  };
+
+  return (
+    <div className="relative flex justify-center items-center min-h-screen bg-gradient-to-b from-blue-100 to-white overflow-hidden">
+      <div className="bg-white shadow-lg rounded-lg flex flex-col md:flex-row items-center p-12 space-y-8 md:space-y-0 md:space-x-12 max-w-6xl relative mt-28 z-20">
+        <div className="absolute top-10 left-20 text-pink-300 text-2xl">★</div>
+        <div className="absolute top-20 right-7 text-blue-300 text-xl">•</div>
+        <div className="absolute bottom-24 left-16 text-purple-300 text-3xl">
+          ✦
+        </div>
+        <div className="absolute bottom-12 right-24 text-pink-300 text-2xl">
+          •
+        </div>
+        <div className="absolute top-3 left-1/2 transform -translate-x-1/2 text-blue-300 text-lg">
+          ✦
+        </div>
+
+        <div className="flex-shrink-0">
+          <img
+            src={passwordImg}
+            alt="Character Illustration"
+            className="w-auto md:w-80 transform hover:scale-105 transition duration-300 ease-in-out"
+          />
+        </div>
+
+        <div className="flex-grow bg-[#F4EBFF] shadow-xl transform hover:scale-105 transition duration-300 ease-in-out w-full sm:w-4/5 md:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto">
+          <div className="bg-purple-50 p-6 sm:p-8 rounded-lg shadow-md">
+            <div>
+              <h2 className="text-xl sm:text-2xl font-bold text-[#000000] mb-2">
+                CREATE PASSWORD
+              </h2>
+              <div className="w-32 sm:w-40 h-0.5 bg-[#F44D99] mb-6" />
+            </div>
+
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div className="relative">
+                <input
+                  type={passwordVisible ? "text" : "password"}
+                  value={password}
+                  id="password"
+                  maxLength={15}
+                  minLength={8}
+                  onChange={handlePasswordChange}
+                  placeholder="Password *"
+                  className={`w-full px-3 sm:px-4 py-2 sm:py-3 border-2 ${
+                    errors.password ? "border-red-500" : "border-purple-300"
+                  } rounded-lg focus:outline-none focus:ring-2 ${
+                    errors.password
+                      ? "focus:ring-red-500"
+                      : "focus:ring-purple-500"
+                  }`}
+                />
+                <div
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                  onClick={togglePasswordVisibility}
+                >
+                  {passwordVisible ? (
+                    <FaEyeSlash className="text-gray-500 w-5 h-5" />
+                  ) : (
+                    <FaEye className="text-gray-500 w-5 h-5" />
+                  )}
+                </div>
+                {errors.password && (
+                  <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+                )}
+              </div>
+
+              <div className="relative">
+                <input
+                  type={confirmPasswordVisible ? "text" : "password"}
+                  value={confirmPassword}
+                  id="confirmPassword"
+                  onChange={handleConfirmPasswordChange}
+                  maxLength={15}
+                  minLength={8}
+                  placeholder="Confirm Password *"
+                  className={`w-full px-3 sm:px-4 py-2 sm:py-3 border-2 ${
+                    errors.confirmPassword
+                      ? "border-red-500"
+                      : "border-purple-300"
+                  } rounded-lg focus:outline-none focus:ring-2 ${
+                    errors.confirmPassword
+                      ? "focus:ring-red-500"
+                      : "focus:ring-purple-500"
+                  }`}
+                />
+                <div
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                  onClick={toggleConfirmPasswordVisibility}
+                >
+                  {confirmPasswordVisible ? (
+                    <FaEyeSlash className="text-gray-500 w-5 h-5" />
+                  ) : (
+                    <FaEye className="text-gray-500 w-5 h-5" />
+                  )}
+                </div>
+                {errors.confirmPassword && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.confirmPassword}
+                  </p>
+                )}
+              </div>
+              <div className="relative">
+                <select
+                  value={selectedOption}
+                  onChange={(e) => setSelectedOption(e.target.value)}
+                  className={`w-full px-3 sm:px-4 py-2 sm:py-3 border-2 ${
+                    errors.dropdown ? "border-red-500" : "border-purple-300"
+                  } rounded-lg focus:outline-none focus:ring-2 ${
+                    errors.dropdown
+                      ? "focus:ring-red-500"
+                      : "focus:ring-purple-500"
+                  }`}
+                >
+                  <option value="">How did you hear about us?</option>
+                  <option value="Social Media">Social Media</option>
+                  <option value="Friends">Friends</option>
+                  <option value="Advertisements">Advertisements</option>
+                </select>
+                {errors.dropdown && (
+                  <p className="text-red-500 text-sm mt-1">{errors.dropdown}</p>
+                )}
+              </div>
+
+              <div className="flex justify-center mt-4">
+                <button
+                  type="submit"
+                  className="w-36 sm:w-48 py-2 sm:py-3 bg-[#FFFFFF] text-[#000000] font-bold rounded-md border-2 hover:border-blue-300 shadow-xl flex items-center justify-center hover:bg-purple-500 hover:text-white transition duration-300"
+                >
+                  Sign Up
+                  <IoArrowForwardOutline className="ml-2" />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-0 left-0 w-full mb-48">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1920"
+          height="536"
+          viewBox="0 0 1920 536"
+          fill="none"
+          className="w-full h-auto"
+        >
+          <path
+            d="M-857 185.27L-733.417 231.924C-609.833 279.454 -362.667 371.447 -115.5 348.887C131.667 325.451 378.833 185.27 626 161.834C873.167 139.274 1120.33 231.267 1367.5 220.316C1614.67 209.364 1861.83 91.0868 1985.42 33.4815L2109 -25V535.721H1985.42C1861.83 535.721 1614.67 535.721 1367.5 535.721C1120.33 535.721 873.167 535.721 626 535.721C378.833 535.721 131.667 535.721 -115.5 535.721C-362.667 535.721 -609.833 535.721 -733.417 535.721H-857V185.27Z"
+            fill="url(#paint0_radial_377_608)"
+          />
+          <defs>
+            <radialGradient
+              id="paint0_radial_377_608"
+              cx="0"
+              cy="0"
+              r="1"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="translate(257 -506.059) rotate(90) scale(1002.96 5305.27)"
+            >
+              <stop offset="0.266" stopColor="#26A3C9" />
+              <stop offset="1" stopColor="#FAF6FF" />
+            </radialGradient>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePassword;
