e39fd3d3ff46a7bdf98c913a7d9c538ee71dc2bd
Saikumar1-rf <saikumar.rfchh@gmail.com>
2024-12-19 21:28:08 +0530

    all are cleared

--- gruhapandit-new/vite.config.js
+++ gruhapandit-new/vite.config.js
@@ -1,27 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{port:3060}
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:3060},
-  build: {
-    outDir: 'dist', // Ensure this matches your Publish Directory
-  },
-});
-
-
-
-
-
-
-
+  server:{port:3060}
+})
