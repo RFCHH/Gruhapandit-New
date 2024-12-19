557bff6bb01646626ab299606e20e1c2d919d16c
Saikumar1-rf <saikumar.rfchh@gmail.com>
2024-12-19 20:36:34 +0530

    vite.config

--- gruhapandit-new/vite.config.js
+++ gruhapandit-new/vite.config.js
@@ -1,8 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{port:3060}
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:3060}
+  server:{port:3060},
+  base:'./'
 })
+
+
+
+
+
+
+
