1537d569f6d11b549908f16505172049af6ebf0f
sanath6 <sanath.rfchh@gmail.com>
2024-12-31 17:40:38 +0530

    Protected Routes
    App(Protected Routes), Authorized access folder

--- gruhapandit-new/src/App.jsx
+++ gruhapandit-new/src/App.jsx
@@ -33,14 +33,18 @@ import Subjects from "./Dashboard/Subject";
 import AllEmailTemplatesPage from "./Email/AllEmailTemplatesPage";
 import EditEmailTemplatePage from "./Email/EditEmailTemplatePage";
 import Payment from "./Payment";
-
 import Exam from "./Exam/SelfExam";
 import ExamPopUp from "./Exam/ExamPopUp";
-
 import RequestApproval from "./Admin Flow/Request";
+import Unauthorized from "./Authorized Access/Unauthorized";
+import Maintaience from './Authorized Access/undermaintaience';
+import ProtectedRoute from "./Authorized Access/ProtectedRoute";
+
 
 
 function Layout({ children }) {
+
+  
   const location = useLocation();
   const isAuthenticated = true;
 
@@ -58,12 +62,10 @@ function Layout({ children }) {
     "/Registration",
     "/TutorTable",
     "/StudentTable",
-
     '/email-templates',
     '/Payment',
     '/subject',
     '/SelfExam',
-
     "/Request",
 
   ];
@@ -81,58 +83,78 @@ function Layout({ children }) {
 }
 
 function App() {
+   
   return (
     <>
       <Router>
         <Layout>
           <Routes>
+             {/* public access path */}
             <Route path="/" element={<Home />} />
             <Route path="/About" element={<About />} />
             <Route path="/OurServices" element={<Services />} />
             <Route path="/Reviews" element={<Reviews />} />
             <Route path="/ContactUs" element={<Contact />} />
+            
+            <Route path="/LoginPage" element={<Login />}></Route>
             <Route path="/userselection" element={<UserSelection />}></Route>
             <Route path="/CreatePassword" element={<CreatePassword />}></Route>
             <Route path="/SignUp" element={<SignUp />}></Route>
-            <Route path="/LoginPage" element={<Login />}></Route>
             <Route path="/ForgotPassword" element={<ForgotPassword />} />
-            <Route
-              path="/PasswordVerification"
-              element={<PasswordVerification />}
-            />
+            <Route path="/PasswordVerification" element={<PasswordVerification />}/>
             <Route path="/successfull" element={<LoginSuccess />} />
+            <Route path="/unauthorized" element={<Unauthorized />} />
+            <Route path="/undermaintaience" element={< Maintaience />} /> 
+            
+          {/*  TUTOR AND STUDENT ACCESS WITH Regular and premium  */}
+            <Route element={<ProtectedRoute allowedRoles={[
+                { userRole: "ROLE_PREMIUM_USER", role: "TUTOR" },
+                { userRole: "ROLE_PREMIUM_USER", role: "STUDENT" },
+                { userRole: "ROLE_REGULAR_USER", role: "TUTOR" },
+                { userRole: "ROLE_REGULAR_USER", role: "STUDENT" } ]}/>}>
+        
+                <Route path="/Dashboard/:userId" element={<Dashboard />} />
+                <Route path="/Profile/:userId" element={<TutorProfile />} />
+                <Route path="/Payment" element={<Payment/>}></Route>
+            </Route> 
 
-            <Route path="/Dashboard/:userId" element={<Dashboard />} />
-            <Route path="/National/:userId" element={<National />} />
-            <Route path="/Profile/:userId" element={<TutorProfile />} />
-            <Route path="/Request/:userId" element={<MyRequest />} />
-            <Route path="/createReviews" element={<CreateReview />} />
-            <Route path="review" element={<Review />} />
-            <Route path="/plans/:userId" element={<MyPlans />} />
-            <Route path="/Feeds/:userId" element={<StudentFeed />} />
-            <Route path="/ExamList"element={<ExamList/>}></Route>
-            <Route path="/subject/:userId" element={<Subjects/>}></Route>
-            <Route path="/Payment" element={<Payment/>}></Route>
-            <Route path="/SelfExam" element={<Exam/>}></Route>
-            <Route path="/exampopup" element={<ExamPopUp/>}/>
+              
+          {/*  TUTOR AND STUDENT ACCESS with PREMIMUM USER */}
+            <Route element={<ProtectedRoute allowedRoles={[
+              { userRole: "ROLE_PREMIUM_USER", role: "TUTOR" },
+              { userRole: "ROLE_PREMIUM_USER", role: "STUDENT" }]} />}>
+
+              <Route path="/createReviews" element={<CreateReview />} />
+              <Route path="/review" element={<Review />} />
+              <Route path="/Request/:userId" element={<MyRequest />} />
+              <Route path="/plans/:userId" element={<MyPlans />} />
+              <Route path="/Feeds/:userId" element={<StudentFeed />} />
+              <Route path="/subject/:userId" element={<Subjects/>}/>
+            </Route> 
+
+          
+          {/*  TUTOR ACCESS with PREMIMUM USER */}
+          <Route element={<ProtectedRoute allowedRoles={[{ userRole: "ROLE_PREMIUM_USER", role: "TUTOR" }]} />}>
+              <Route path="/National/:userId" element={<National />} />
+              <Route path="/ExamList"element={<ExamList/>}></Route>
+              <Route path="/exampopup" element={<ExamPopUp/>}/>
+            </Route> 
+
+          {/*  STUDENT ACCESS with PREMIMUM USER */}
+          <Route element={<ProtectedRoute allowedRoles={[{ userRole: "ROLE_PREMIUM_USER", role: "STUDENT" }]} />}>
+              <Route path="/SelfExam" element={<Exam/>}></Route>
+          </Route> 
             
 
-
-
-
+          {/* admin access path */}
+          <Route element={<ProtectedRoute allowedRoles={["ROLE_ADMIN"]} />}>
             <Route path="/Registration" element={<Registration />} />
             <Route path="/TutorTable" element={<TutorTable />} />
             <Route path="/StudentTable" element={<StudentTable />} />
             <Route path="/Request" element={<RequestApproval />} />
-
-            <Route
-              path="/email-templates"
-              element={<AllEmailTemplatesPage />}
-            />
-            <Route
-              path="/edit-email-template/:templateId"
-              element={<EditEmailTemplatePage />}
-            />
+            <Route path="/email-templates" element={<AllEmailTemplatesPage />}/>
+            <Route path="/edit-email-template/:templateId" element={<EditEmailTemplatePage />}/>
+          </Route>
           </Routes>
         </Layout>
       </Router>
